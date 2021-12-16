654762f782d2916861c5fcd4e138c776cdb9db39
YASHWANTH KIRAN S <yashwanths.cs19@bmsce.ac.in>
2021-12-16 09:57:26 +0530

    Add files via upload

---
+++ LAB 15 NUMBER OF VOWELS/vowels.sh
@@ -0,0 +1,15 @@
+echo "Entre a string to find the number of Vowels "
+read st
+len=`expr $st | wc -c`
+len=`expr $len - 1`
+count=0
+while [ $len -gt 0 ]
+do
+ch=`expr $st | cut -c $len`
+case $ch in
+
+[aeiou,AEIOU]) count=`expr $count + 1` ;;
+esac
+len=`expr $len - 1`
+done
+echo "Number of vowels Is $count"
